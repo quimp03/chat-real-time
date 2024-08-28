1bdd6926c44c30c87a7a8fbddab20d668a2a21d4
Mai Phuc Qui <maiphucqui8482@gmail.com>
2024-08-28 23:31:44 +0700

    Cài dặt

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const database = require("./config/database");
+const systemConfig = require("./config/system");
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
+const flash = require('express-flash');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const moment = require("moment");
+const path = require('path');
+const http = require('http');
+const { Server } = require("socket.io");
+dotenv.config();
+
+database.connect();
+const routeAdmin = require("./routes/admin/index.route");
+const routeClient = require("./routes/client/index.route");
+const app = express();
+const port = process.env.PORT;
+
+// SocketIO
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log("Có 1 user kết nối");
+})
+// End SocketIO
+
+// Flash
+app.use(cookieParser('JHSVBDSDSD'));
+app.use(session({ cookie: { maxAge: 60000 }}));
+app.use(flash());
+// End Flash
+app.set('view engine', 'pug');
+app.set('views', `${__dirname}/views`);
+app.use(express.static(`${__dirname}/public`));
+app.use(methodOverride('_method'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// App Locals Variable
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
+app.locals.moment = moment;
+/* New Route to the TinyMCE Node module */
+app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce')));
+// Routes
+routeAdmin(app);
+routeClient(app);
+app.get("*", (req, res) => {
+  res.render("client/pages/errors/404", {
+    pageTitle: "404 Not Found",
+  });
+});
+server.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+  });
