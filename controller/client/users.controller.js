6436add1adb57f8ed8fc3a2a3da84ab7f59d8df5
Mai Phuc Qui <maiphucqui8482@gmail.com>
2024-08-30 01:05:59 +0700

    Chức năng gửi yêu cầu (Lưu data vào database)

--- controller/client/users.controller.js
+++ controller/client/users.controller.js
@@ -1,7 +1,19 @@
 const User = require("../../models/user.model");
+const usersSocket = require("../../sockets/client/users.socket");
 // [GET] /users/not-friend
 module.exports.notFriend = async (req, res) => {
+     // SocketIO
+  usersSocket(req, res);
+  // End SocketIO
+    const userId = res.locals.user.id;
+
+  const users = await User.find({
+    _id: { $ne: userId }, // not equal
+    status: "active",
+    deleted: false,
+  }).select("avatar fullName");
     res.render("client/pages/users/not-friend", {
       pageTitle: "Danh sách người dùng",
+      users:users
     });
   };
