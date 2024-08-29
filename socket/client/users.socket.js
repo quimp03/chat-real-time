285b307c3ad9df81e6db4f785bb62a43beb2a562
Mai Phuc Qui <maiphucqui8482@gmail.com>
2024-08-30 01:16:25 +0700

    Chức năng chấp nhận kết bạn

--- socket/client/users.socket.js
+++ socket/client/users.socket.js
@@ -76,5 +76,38 @@ module.exports = (req, res) => {
           $pull: { requestFriends: userIdA }
         });
       })
+       // Khi A chấp nhận kết bạn của B
+    socket.on("CLIENT_ACCEPT_FRIREND", async (userIdB) => {
+        console.log(userIdA);
+        console.log(userIdB);
+  
+        // Thêm {user_id, room_chat_id} của B vào friendsList của A
+        // Xóa id của B trong acceptFriends của A
+        await User.updateOne({
+          _id: userIdA
+        }, {
+          $push: {
+            friendsList: {
+              user_id: userIdB,
+              room_chat_id: ""
+            }
+          },
+          $pull: { acceptFriends: userIdB }
+        });
+  
+        // Thêm {user_id, room_chat_id} của A vào friendsList của B
+        // Xóa id của A trong requestFriends của B
+        await User.updateOne({
+          _id: userIdB
+        }, {
+          $push: {
+            friendsList: {
+              user_id: userIdA,
+              room_chat_id: ""
+            }
+          },
+          $pull: { requestFriends: userIdA }
+        });
+      })
   })
 }
